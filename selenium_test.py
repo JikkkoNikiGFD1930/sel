b3a3211b79cdd9cea81134f3e62f3ce68150a20d
JikkkoNikiGFD1930 <148345531+JikkkoNikiGFD1930@users.noreply.github.com>
2025-04-12 12:16:08 -0300

    Update selenium_test.py

--- selenium_test.py
+++ selenium_test.py
@@ -1,13 +1,16 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 
 options = Options()
+options.binary_location = "/usr/bin/chromium-browser"
 options.add_argument("--headless")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
 
-driver = webdriver.Chrome(options=options)
-driver.get("https://1fichier.com/")
+service = Service("/usr/bin/chromedriver")
+driver = webdriver.Chrome(service=service, options=options)
 
+driver.get("https://1fichier.com/")
 driver.save_screenshot("screenshot.png")
 driver.quit()
