5415d8c2a763fc6eb5c8ab4552bba3cf350b9501
JikkkoNikiGFD1930 <148345531+JikkkoNikiGFD1930@users.noreply.github.com>
2025-04-12 12:12:41 -0300

    Update selenium_test.py

--- selenium_test.py
+++ selenium_test.py
@@ -2,19 +2,12 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 options = Options()
-options.add_argument('--headless')
-options.add_argument('--no-sandbox')
-options.add_argument('--disable-dev-shm-usage')
+options.add_argument("--headless")
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
 
 driver = webdriver.Chrome(options=options)
-
-# Load the website
 driver.get("https://1fichier.com/")
 
-# Wait a little if needed (optional)
-driver.implicitly_wait(10)
-
-# Save screenshot
 driver.save_screenshot("screenshot.png")
-
 driver.quit()
